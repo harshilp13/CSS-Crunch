97843fb8ac792114070c5286443f6f23a392f347
RJ105 <49883388+RJ105@users.noreply.github.com>
2020-10-07 22:24:11 +0530

    adding npr functions

--- CSS_crunch.py
+++ CSS_crunch.py
@@ -1,3 +1,33 @@
+# Function to compute value of npr 
+import math 
+def fact(n):  
+    if (n <= 1): 
+        return 1
+          
+    return n * fact(n - 1)  
+  
+def PermutationCoeff(n, r):  
+      
+    return math.floor(fact(n) /
+                fact(n - r))
+
+ 
+
+def read_through_file():
+  ch_list = []
+  filename = input("ENter the filename = ")
+  # if (!(exists(filename))):
+  # #   print("Invalid filename\nExitting program")
+  # #   exit()
+  f= open(filename,"r")
+  f.read()
+  for _ in f:
+    ch_list.append(_)
+  f.close()
+  return ch_list
+
+
+
 if __name__ == '__main__':
   ch = int(input("Enter input via 1.File 2.Runtime : "))
   # If user wishes to enter input via input file
